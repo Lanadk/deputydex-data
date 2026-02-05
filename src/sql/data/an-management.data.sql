b398134ffdd48514845011bdd2dcc5fb81a68047
Lanadk <ottmanbecuwe@gmail.com>
2026-02-05 14:13:49 +0100

    an-data-management tables for current legislature

--- src/sql/data/an-management.data.sql
+++ src/sql/data/an-management.data.sql
@@ -1,6 +1,12 @@
-INSERT INTO param_legislatures (number, is_current)
-VALUES (16, false),
-       (17, true);
+INSERT INTO param_legislatures (number)
+VALUES (16),
+       (17);
+
+INSERT INTO param_current_legislatures (legislature_id, number)
+VALUES ((SELECT id
+         FROM param_legislatures
+         WHERE number = 17),
+        17);
 
 
 INSERT INTO ref_data_domains (code, description)
@@ -10,32 +16,22 @@ VALUES ('acteurs', 'Personne physique'),
 
 -- Insérer les sources de données pour la législature 16 (archive)
 INSERT INTO param_data_sources (domain_id, legislature_id, download_url, file_name)
-VALUES
-    (
-        (SELECT id FROM ref_data_domains WHERE code = 'acteurs'),
+VALUES ((SELECT id FROM ref_data_domains WHERE code = 'acteurs'),
         (SELECT id FROM param_legislatures WHERE number = 16),
         'https://data.assemblee-nationale.fr/static/openData/repository/16/amo/acteurs_mandats_organes_divises/AMO50_acteurs_mandats_organes_divises.json.zip',
-        'AMO50_acteurs_mandats_organes_divises.json.zip'
-    ),
-    (
-        (SELECT id FROM ref_data_domains WHERE code = 'votes'),
+        'AMO50_acteurs_mandats_organes_divises.json.zip'),
+       ((SELECT id FROM ref_data_domains WHERE code = 'votes'),
         (SELECT id FROM param_legislatures WHERE number = 16),
         'https://data.assemblee-nationale.fr/static/openData/repository/16/loi/scrutins/Scrutins.json.zip',
-        'Scrutins.json.zip'
-    );
+        'Scrutins.json.zip');
 
 -- Insérer les sources de données pour la législature 17 (current)
 INSERT INTO param_data_sources (domain_id, legislature_id, download_url, file_name)
-VALUES
-    (
-        (SELECT id FROM ref_data_domains WHERE code = 'acteurs'),
+VALUES ((SELECT id FROM ref_data_domains WHERE code = 'acteurs'),
         (SELECT id FROM param_legislatures WHERE number = 17),
         'https://data.assemblee-nationale.fr/static/openData/repository/17/amo/acteurs_mandats_organes_divises/AMO50_acteurs_mandats_organes_divises.json.zip',
-        'AMO50_acteurs_mandats_organes_divises.json.zip'
-    ),
-    (
-        (SELECT id FROM ref_data_domains WHERE code = 'votes'),
+        'AMO50_acteurs_mandats_organes_divises.json.zip'),
+       ((SELECT id FROM ref_data_domains WHERE code = 'votes'),
         (SELECT id FROM param_legislatures WHERE number = 17),
         'https://data.assemblee-nationale.fr/static/openData/repository/17/loi/scrutins/Scrutins.json.zip',
-        'Scrutins.json.zip'
-    );
+        'Scrutins.json.zip');
--- src/sql/schema/an-management.schema.sql
+++ src/sql/schema/an-management.schema.sql
@@ -2,13 +2,23 @@
 CREATE TABLE param_legislatures
 (
     id         SERIAL PRIMARY KEY,
-    number     INT     NOT NULL UNIQUE,
-    is_current BOOLEAN NOT NULL DEFAULT FALSE,
+    number     INT NOT NULL UNIQUE,
     start_date DATE,
     end_date   DATE,
-    created_at TIMESTAMP        DEFAULT NOW(),
-    updated_at TIMESTAMP        DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
+
+--Table de parametrage de la legislature en cours (une seule ligne possible en base)
+CREATE TABLE param_current_legislatures
+(
+    id             SERIAL PRIMARY KEY,
+    legislature_id INT NOT NULL REFERENCES param_legislatures (id),
+    number         INT NOT NULL UNIQUE,
+    updated_at     TIMESTAMP DEFAULT NOW()
+);
+CREATE UNIQUE INDEX only_one_row
+    ON param_current_legislatures ((true));
 
 -- Table de refentiel des domains métier qu'on exploite
 CREATE TABLE ref_data_domains
@@ -22,10 +32,10 @@ CREATE TABLE ref_data_domains
 CREATE TABLE param_data_sources
 (
     id             SERIAL PRIMARY KEY,
-    domain_id      INT     NOT NULL REFERENCES ref_data_domains (id),
-    legislature_id INT     NOT NULL REFERENCES param_legislatures (id),
-    download_url   TEXT    NOT NULL,
-    file_name      TEXT    NOT NULL,
+    domain_id      INT  NOT NULL REFERENCES ref_data_domains (id),
+    legislature_id INT  NOT NULL REFERENCES param_legislatures (id),
+    download_url   TEXT NOT NULL,
+    file_name      TEXT NOT NULL,
     created_at     TIMESTAMP DEFAULT NOW(),
 
     UNIQUE (domain_id, legislature_id)
