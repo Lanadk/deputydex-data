d75b5421d817cd9e0a604e860e5bfe74e535e9f9
Lanadk <ottmanbecuwe@gmail.com>
2026-02-06 15:27:07 +0100

    fix: packaging

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import {prisma} from "../src/db/prisma";
+import {prisma} from "./prisma";
 
 async function main() {
     console.log('🌱 Seeding database...\n');
