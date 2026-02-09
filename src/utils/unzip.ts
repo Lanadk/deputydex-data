5b8fa364273c1159fab16ba4597fbf5b27b35ab4
Lanadk <ottmanbecuwe@gmail.com>
2026-02-09 21:15:09 +0100

    Unzip optimization, unzipper > adm-zip

--- src/utils/unzip.ts
+++ src/utils/unzip.ts
@@ -1,16 +1,25 @@
 import fs from "fs";
-import AdmZip from "adm-zip";
+import unzipper from "unzipper";
 
 export async function unzipFile(zipPath: string, targetDir: string): Promise<void> {
     if (!fs.existsSync(zipPath)) {
         throw new Error(`Zip file does not exist: ${zipPath}`);
     }
 
-    // Créer le dossier cible si nécessaire
+    // Créer le dossier cible si pas fait
     if (!fs.existsSync(targetDir)) {
         fs.mkdirSync(targetDir, { recursive: true });
     }
 
-    const zip = new AdmZip(zipPath);
-    zip.extractAllTo(targetDir, true);
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(zipPath)
+            .pipe(unzipper.Extract({ path: targetDir }))
+            .on('close', () => {
+                //Attendre un peu
+                setTimeout(() => resolve(), 5000);
+            })
+            .on('error', (error) => {
+                reject(new Error(`Unzip failed: ${error.message}`));
+            });
+    });
 }
