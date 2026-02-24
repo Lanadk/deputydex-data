875f61771d07ea424077755d0cd3e4c55bcaab4d
Lanadk <ottmanbecuwe@gmail.com>
2026-02-24 17:02:55 +0100

    update env

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv"
 import { existsSync } from "fs"
 
 // Charger .env.local en local
-if (!process.env.DB_URL && existsSync('.env.local')) {
-    config({ path: '.env.local' })
+if (!process.env.DB_URL && existsSync('../.env.local')) {
+    config({ path: '../.env.local' })
     console.log('📄 Variables chargées depuis .env.local')
 }
